6e7b06fff08cc41d79219545c91b2f6fa1b02b32
Seamus Barcoe <C08502846@mydit.ie>
2013-03-27 16:01:34 +0000

    Add in picture, clean up html page and servlet

--- assignment/src/ie/dit/barcoe/seamus/AssignmentServlet.java
+++ assignment/src/ie/dit/barcoe/seamus/AssignmentServlet.java
@@ -1,7 +1,14 @@
 package ie.dit.barcoe.seamus;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.security.Principal;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class AssignmentServlet extends HttpServlet {
@@ -9,5 +16,24 @@ public class AssignmentServlet extends HttpServlet {
 			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world333");
+		
+		UserService userService = UserServiceFactory.getUserService();
+		Principal myPrincipal = req.getUserPrincipal();
+		String emailAddress = null;
+		String thisURL = req.getRequestURI();
+		String loginURL = userService.createLoginURL(thisURL);
+		String logoutURL = userService.createLogoutURL(thisURL);
+		resp.setContentType("text/html");
+		if(myPrincipal == null) {
+		resp.getWriter().println("<p>You are Not Logged In time</p>");
+		resp.getWriter().println("<p>You can <a href=\""+loginURL+
+		"\">sign in here</a>.</p>");
+		} // end if not logged in
+		if(myPrincipal !=null) {
+		emailAddress = myPrincipal.getName();
+		resp.getWriter().println("<p>You are Logged in as (email): "+emailAddress+"</p>");
+		resp.getWriter().println("<p>You can <a href=\"" + logoutURL +
+		"\">sign out</a>.</p>");
+		} // end if logged in
 	}
 }
