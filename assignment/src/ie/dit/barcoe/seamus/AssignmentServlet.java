3ade8cb084b0fdd2b27c369587af5a02648817c2
Seamus Barcoe <C08502846@mydit.ie>
2013-03-27 15:45:03 +0000

    Test

--- assignment/src/ie/dit/barcoe/seamus/AssignmentServlet.java
+++ assignment/src/ie/dit/barcoe/seamus/AssignmentServlet.java
@@ -8,6 +8,6 @@ public class AssignmentServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, world111");
 	}
 }
