bdb8f5f39a1ba3051dec29eb2ec3270b7517614b
Seamus Barcoe <C08502846@mydit.ie>
2013-03-27 13:53:31 +0000

    Test3

---
+++ assignment/src/ie/dit/barcoe/seamus/AssignmentServlet.java
@@ -0,0 +1,13 @@
+package ie.dit.barcoe.seamus;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class AssignmentServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
