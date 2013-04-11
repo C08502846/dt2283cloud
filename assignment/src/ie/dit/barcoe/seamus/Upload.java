fa891ec7da5422ff319759e5c07b4a4d655df7c1
Seamus Barcoe <C08502846@mydit.ie>
2013-04-11 16:18:14 +0100

    Successful queries made,

--- assignment/src/ie/dit/barcoe/seamus/Upload.java
+++ assignment/src/ie/dit/barcoe/seamus/Upload.java
@@ -3,7 +3,6 @@ package ie.dit.barcoe.seamus;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Map;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,13 +13,15 @@ import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.images.ImagesService;
-import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.datastore.Key;
+
+import com.google.appengine.api.images.*;
+
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -28,64 +29,50 @@ import com.google.appengine.api.users.UserServiceFactory;
 public class Upload extends HttpServlet 
 
 {
-	private static final Logger log = Logger.getLogger(Upload.class.getName());
 	private static final long serialVersionUID = 1L;
 	private BlobstoreService blobstoreService =	BlobstoreServiceFactory.getBlobstoreService();
 	private BlobInfoFactory bif = new BlobInfoFactory();
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException 
-			{
-		        UserService userService = UserServiceFactory.getUserService();
-		        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		        ImagesService ImageServiceObject = ImagesServiceFactory.getImagesService();
-		        
-		        @SuppressWarnings("deprecation")
-		        Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);		        
-		        BlobKey blobKey = blobs.get("myFile");  
-		        
-		        if (blobKey == null) 
-		        {
-		        	res.sendRedirect("/");
-		        } 
-		        else
-		        {
-		        	User user = userService.getCurrentUser();
-		            Date date = new Date();	
-		            String description = req.getParameter("description");
-		            String imageUrl =ImageServiceObject.getServingUrl(blobKey);
-		            String imageName  = bif.loadBlobInfo(blobKey).getFilename();			        
-		            String uploadImageFoo = "foo";					
-					
-			        Key imageKey = KeyFactory.createKey("image", imageName); // create a primary Key for datastore entity called imageName 
-			       				
-					Entity UploadImageEntity = new Entity("UploadedImage", imageName);	
-			        	       
-			        UploadImageEntity.setProperty("user", user); // Setting the properties of Entity called UploadImageEntity
-			        UploadImageEntity.setProperty("date", date);
-			        UploadImageEntity.setProperty("description", description);			        
-					UploadImageEntity.setProperty("imageUrl", imageUrl);
-					UploadImageEntity.setProperty("imageName", imageName);
-					UploadImageEntity.setProperty("blobkey", blobKey);
-			       
-			        datastore.put(UploadImageEntity);				
-					
-		        	System.out.println("Uploaded a file with blobKey: "+blobKey.getKeyString()); // printing blobkey 
-		        	System.out.println("UploadedImageEntity: "+UploadImageEntity); // Printing Entity to console
-		        	res.sendRedirect("/serve?blob-key=" + blobKey.getKeyString());		        	
-		        }
-		        
-//		        String content = req.getParameter("content");
-//		        if (content == null) {
-//		            content = "(No greeting)";
-//		        }
-//		        if (user != null) {
-//		            log.info("Comment posted by user " + user.getNickname() + ": " + content);
-//		        } else {
-//		            log.info("Comment posted anonymously: " + content);
-//		        }
-//		        res.sendRedirect("/index.jsp");
-		        
-		        
-}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		@SuppressWarnings("deprecation")
+		Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);
+		BlobKey blobKey = blobs.get("imageFile");
+		
+		//If upload fails return message
+		if (blobKey == null) 
+		{
+			resp.sendRedirect("/index.hmtl");
+		} 
+		else
+		{
+			ImagesService ImageServiceObject = ImagesServiceFactory.getImagesService(); 
+			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	        UserService userService = UserServiceFactory.getUserService();
+	        User user = userService.getCurrentUser();  	        
+			String viewImages = "/index.jsp";
+            Date date = new Date();	
+            String description = req.getParameter("description");
+            String imageUrl =ImageServiceObject.getServingUrl(blobKey);
+            String imageName  = bif.loadBlobInfo(blobKey).getFilename();			        
+            String uploadImageFoo = "foo";	
+
+			Key uploadImageKey = KeyFactory.createKey("image", uploadImageFoo);
+			
+			Entity UploadImageEntity = new Entity("UploadImage", uploadImageKey);
+						
+			UploadImageEntity.setProperty("blobkey", blobKey);
+			UploadImageEntity.setProperty("imageUrl", imageUrl);
+			UploadImageEntity.setProperty("imageName", imageName);
+			UploadImageEntity.setProperty("description", description);
+			UploadImageEntity.setProperty("date", date);
+			UploadImageEntity.setProperty("user", user);
+			
+			datastore.put(UploadImageEntity);
+
+			resp.sendRedirect(viewImages); // redirect so that user is left on index.jsp when file is uploaded
+		}
+
+	}
 }
